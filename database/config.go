a1c360470c279a00f7c2f05827e2c7732a080754
irat <iratdya.winggari@agriaku.com>
2022-08-06 10:14:26 +0700

    add setting environment

--- database/config.go
+++ database/config.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
@@ -9,11 +10,11 @@ import (
 
 // SetupDB : initializing mysql database
 func SetupDB() *gorm.DB {
-	USER := "devrank"
-	PASS := "devrank"
-	HOST := "localhost"
+	USER := os.Getenv("MYSQL_USER")
+	PASS := os.Getenv("MYSQL_PASSWORD")
+	HOST := os.Getenv("MYSQL_HOST")
 	PORT := "3306"
-	DBNAME := "devrank"
+	DBNAME := os.Getenv("MYSQL_DBNAME")
 	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
 	db, err := gorm.Open("mysql", URL)
 	if err != nil {
